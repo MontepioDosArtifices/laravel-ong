b108d8d898503e95a04fe307f17c2290a84af7f7
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-06-26 09:13:57 -0300

    :twisted_rightwards_arrows: Fixed conflicts in routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/doar', 'Services\PagarmeRequestService@createForm')->name('donation
 Route::post('/doar/enviado', 'Services\PagarmeRequestService@createTransaction')->name('donation.send');
 
 Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+Route::get('/dashboard/calendario', function () { return view('calendar'); })->name('calendar');
 
 Route::get('/dashboard/login/', function () { return view('user-login'); })->name('user.login');
 Route::post('/dashboard/login/enviado', 'LoginController@index')->name('user.login.send');
