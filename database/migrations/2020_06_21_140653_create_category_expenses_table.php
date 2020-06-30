744bced33e5f6f0a94e7493019cd0113223a2ba6
gcairesdev <pessoalgcaires@gmail.com>
2020-06-30 14:01:18 -0300

    :bug: Expenses migrations fixed

--- database/migrations/2020_06_21_140653_create_category_expenses_table.php
+++ database/migrations/2020_06_21_140653_create_category_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoryExpenses extends Migration
+class CreateCategoryExpensesTable extends Migration
 {
   /**
    * Run the migrations.
